cae36721a7cfd53e263a954c1418e4baf098d5c0
Aman Bhardwaj <amanthecool912@gmail.com>
2020-07-16 13:11:39 +0530

    removing all dirs

--- taskmate/todo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from todo import models
-
-# Register your models here.
-
-
-admin.site.register([
-    models.TaskList
-])
--- taskmate/todo/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from todo.models import TaskList
-
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model = TaskList
-        fields =['task','done']
